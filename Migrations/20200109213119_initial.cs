8e5d6b02570d46ceee54edd46d114df5b99d5679
Harold Espinoza <haroldesptru@gmail.com>
2020-01-09 20:18:16 -0600

    Actualizadas las migraciones

---
+++ Migrations/20200109213119_initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FinancecalcServer.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clasificacion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clasificacion", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subclasificacion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ClasificationId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subclasificacion", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subclasificacion_Clasificacion_ClasificationId",
+                        column: x => x.ClasificationId,
+                        principalTable: "Clasificacion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cuenta",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    SubclasificationId = table.Column<int>(nullable: true),
+                    Document = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuenta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cuenta_Subclasificacion_SubclasificationId",
+                        column: x => x.SubclasificationId,
+                        principalTable: "Subclasificacion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cuenta_SubclasificationId",
+                table: "Cuenta",
+                column: "SubclasificationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subclasificacion_ClasificationId",
+                table: "Subclasificacion",
+                column: "ClasificationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cuenta");
+
+            migrationBuilder.DropTable(
+                name: "Subclasificacion");
+
+            migrationBuilder.DropTable(
+                name: "Clasificacion");
+        }
+    }
+}
